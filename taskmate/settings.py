3e0d57b74070c709656718e8949e6b2cf2fee212
mouna <mounae020@gmail.com>
2020-10-12 21:46:02 +0100

    Updated

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -50,7 +50,7 @@ ROOT_URLCONF = 'taskmate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [(os.path.join(BASE_DIR, 'Templates')),],
+        'DIRS': [(os.path.join(BASE_DIR, 'templates')),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
